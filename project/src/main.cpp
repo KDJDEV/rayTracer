ca4eb505f7379064d9c6d7244f529ed5dbce2447
Kyle Dagman <kyle@example.com>
2024-10-27 16:04:58 -0400

    added some utility classes

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -12,15 +15,8 @@ int main() {
     for (int j = 0; j < imageHeight; j++){
         std::clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << std::flush;
         for (int i = 0; i < imageWidth; i++) {
-            auto r = double(i) / (imageWidth - 1);
-            auto g = double(j) / (imageHeight - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            imageFile << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixelColor = color(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0);
+            writeColor(imageFile, pixelColor);
         }
     }
     
